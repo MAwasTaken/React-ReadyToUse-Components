0d9c7e657ac533b53f9c99bc7b9e73026e24353d
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-04-20 16:54:56 +0330

    States Created

--- Hide & Show Password/src/App.jsx
+++ Hide & Show Password/src/App.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+// react
+import React, { useState } from "react";
+
+// packages
 import { Icon } from "react-icons-kit";
 import { eye } from "react-icons-kit/feather/eye";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
 
+// styles
+
+// components
+
+// app
 function App() {
-	const clickHandeler = () => {};
-  
+	// states
+	const [type, setType] = useState("password");
+	const [icon, setIcon] = useState(eyeOff);
+
+	// functions
+	const clickHandeler = () => {
+		if (type === "password") {
+			setType("text");
+			setIcon(eye);
+		} else {
+			setType("password");
+			setIcon(eyeOff);
+		}
+	};
+
 	return (
 		<div className='wrapper'>
 			<div className='input-field'>
-				<input type='password' />
+				<input type={type} />
 				<span onClick={clickHandeler}>
-					<Icon icon={eye} size={25} />
+					<Icon icon={icon} size={25} />
 				</span>
 			</div>
 		</div>
 	);
 }
 
+// exports
 export default App;

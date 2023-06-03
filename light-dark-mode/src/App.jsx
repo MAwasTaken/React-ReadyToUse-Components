205f15b886d9cea218390ceb4bba6e531ad69d50
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-06-03 13:10:51 +0330

    Switch Slider Created

---
+++ light-dark-mode/src/App.jsx
@@ -0,0 +1,31 @@
+// react
+import React from 'react';
+
+// app
+function App() {
+	return (
+		<div className='app'>
+			<header className='header'>
+        <div className="container">
+          <div className="nav">
+            <ul>
+              <li>Home</li>
+              <li>About us</li>
+              <li>Contact us</li>
+            </ul>
+            <label className='switch'>
+              <input type="text" />
+              <span className='slider round'></span>
+            </label>
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        
+      </div>
+		</div>
+	);
+}
+
+// exports
+export default App;
